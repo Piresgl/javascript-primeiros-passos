0fef9badefed8f22258739d80ad97e656adfdea7
pires <gabriel120914@gmail.com>
2026-09-18 11:53:34 -0300

    inicio js

---
+++ programacao.js
@@ -0,0 +1,8 @@
+var idade = 17
+var nome = "pires"
+var altura = 1.70
+var cidade = "São Paulo"
+
+console.log("Ola, meu nome é " + nome)
+console.log ("Eu tenho " + idade + " anos")
+console.log ("Eu moro na cidade de " + cidade)
