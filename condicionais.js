e299327d05f6cbb897d37240b2502750d9e15357
pires <gabriel120914@gmail.com>
2026-09-23 10:26:51 -0300

    chuva

---
+++ condicionais.js
@@ -0,0 +1,16 @@
+let = 18
+var maiorDeIdade = idade > 18
+
+//TIPOS PRIMITIVOS
+let numero = 6
+let texto = "meu texto"
+let logicoVerdadeiro = true
+let logicoFalso = false
+
+document.write("Inicio do Programa<br><br>")
+
+if (maiordeidade == true) {
+    document.write("o usuario é maior de idade ");
+}
+
+document.write("<br><br>Fim do Programa");
